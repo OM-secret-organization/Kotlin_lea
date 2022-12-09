d8c5e2aec2e364ec281bc0ebbeae815b89de063f
leaisrevolution <leaisrevolution@gmail.com>
2022-12-09 13:42:21 +0900

    main()함수의 매개변수

---
+++ project/basic/arrayString.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+  printIn(args[0])
+  printIn(args[1])
+  printIn(args[2])
+}
+
+// 프로그램의 실행 진입점임 main()함수에서 매개변수를 통해 프로그램 외부의 인자를 받아들이려면 함수의 선언을 fun main(args: Array<String>) {}과 같이 선언해야 한다.
